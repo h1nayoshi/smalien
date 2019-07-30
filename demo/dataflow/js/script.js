03c0517bde3eab2b85f449547e23cf89b46cfec8
h1nayoshi <h.inayoshi.849@nitech.jp>
2019-07-30 14:51:36 +0900

    updated script.js for executing the analysis

--- demo/dataflow/js/script.js
+++ demo/dataflow/js/script.js
@@ -1,17 +1,28 @@
 const {PythonShell} = require('python-shell');
+const storage = require('electron-json-storage');
+const path = require('path');
 
 const script_btn = document.getElementById('script_btn');
 const output = document.getElementById('terminal_output');
-script_btn.addEventListener('click', (event) => {  
-    const options = {
-      mode: 'text',
-      pythonOptions: ['-u'] // get print results in real-time
-    };
-    let pyshell = new PythonShell(__dirname + '/test.py', options);
-    pyshell.on('message', message => {
-        const pelem = document.createElement('p');
-        pelem.innerText = message;
-        output.insertAdjacentElement('beforeend', pelem);
+script_btn.addEventListener('click', (event) => {
+    storage.get('config', (error, data) => {
+        if (error) throw error;
+        if (Object.keys(data).length !== 0) {
+            const target_path = data.apkFilePath;
+            const smalien_path = path.resolve('../');
+            const options = {
+                mode: 'text',
+                pythonOptions: ['-u'],
+                scriptPath: smalien_path,
+                args: ['-path', smalien_path, target_path]
+            };
+            let pyshell = new PythonShell('main.py', options);
+            pyshell.on('message', message => {
+                const pelem = document.createElement('p');
+                pelem.innerText = message;
+                output.insertAdjacentElement('beforeend', pelem);
+            });
+        };
     });
 });
 
