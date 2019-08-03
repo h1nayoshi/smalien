b29480bcbdcb250c83cdfbf5fd8875a2a71733de
s0r4syl <ckb15002@nitech.jp>
2019-08-03 18:49:44 +0900

    show analize status and rewrite with jquery

--- demo/dataflow/js/script.js
+++ demo/dataflow/js/script.js
@@ -2,31 +2,33 @@ const {PythonShell} = require('python-shell');
 const storage = require('electron-json-storage');
 const path = require('path');
 
-const script_btn = document.getElementById('script_btn');
-const output = document.getElementById('terminal_output');
-script_btn.addEventListener('click', (event) => {
-    $("#terminal_output").empty();
+const output = $('#terminal_output');
+$("#script_btn").on('click', () => {
+    output.empty();
+    $("#load_states").addClass("active");
     storage.get('config', (error, data) => {
         if (error) throw error;
         if (Object.keys(data).length !== 0) {
             const target_path = data.apkFilePath;
             const smalien_path = path.resolve('../');
-            const ppe = document.getElementsByClassName('ui toggle checkbox')[0].children[0].checked
+            const ppe =$('.ui.checkbox').checkbox('is checked');
             if (ppe)
-              args = ['-ppe', '-path', smalien_path, target_path]
-            else 
-              args = ['-path', smalien_path, target_path]
+                args = ['-ppe', '-path', smalien_path, target_path];
+            else
+                args = ['-path', smalien_path, target_path]
             const options = {
                 mode: 'text',
                 pythonOptions: ['-u'],
                 scriptPath: smalien_path,
                 args: args
             };
-            let pyshell = new PythonShell('main.py', options);
+            const pyshell = new PythonShell('main.py', options);
             pyshell.on('message', message => {
-                const elem = document.createElement('pre');
-                elem.innerText = message;
-                output.insertAdjacentElement('beforeend', elem);
+                output.append('<pre>'+ message +'</pre>');
+            });
+            pyshell.end((err, code, signal) => {
+                if (err) throw err;
+                $("#load_states").removeClass("active");
             });
         }
     });
